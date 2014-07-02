c0f45c426124b157c5b177c8048feb77c5a1ebd3
Antonis Tsiapaliokas <antonis.tsiapaliokas@kde.org>
2014-07-02 18:33:47 +0300

    Initial support for angular

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render(res.sendfile('index.html', {
+    root: process.cwd() + '/client'
+  }));
 });
 
 module.exports = router;
