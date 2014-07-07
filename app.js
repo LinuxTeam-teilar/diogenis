a23d0a8e4a59cfdecef7dd587dedb5c8bdcb8b56
Giorgos Tsiapaliokas <giorgos.tsiapaliokas@kde.org>
2014-07-07 13:37:13 +0300

    clean app.js and use redis

--- app.js
+++ app.js
@@ -1,9 +1,5 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('static-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
 
 var app = express();
 
@@ -11,11 +7,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(favicon());
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
-app.use(cookieParser());
+require('./middleware.js');
 
 // TODO remove the public dir
 app.use(express.static(path.join(__dirname, 'public')));
@@ -23,36 +15,5 @@ app.use(express.static(path.join(__dirname, 'client')));
 
 require('./routes/routes.js');
 
-/// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
-
-/// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
-});
-
-
 module.exports = app;
+
---
+++ middleware.js
@@ -0,0 +1,31 @@
+var ini = require('ini');
+var fs = require('fs')
+var express = require('express');
+var path = require('path');
+var favicon = require('static-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+var RedisStore = require('connect-redis')(session);
+
+var app = express();
+
+var config = ini.parse(fs.readFileSync(process.cwd() + '/diogenis.conf', 'utf-8'))
+
+app.use(favicon());
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(cookieParser());
+
+var options = {
+    host: config.redis.redis_host,
+    prefix: config.redis.redis_prefix
+}
+
+app.use(session({ store: new RedisStore(options),
+                  secret: config.redis.redis_cookie_secret,
+                  saveUninitialized: true,
+                  resave: true }));
+
