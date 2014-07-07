a4d791071ab555c24f8cb8738a26851df1cc2795
Giorgos Tsiapaliokas <giorgos.tsiapaliokas@kde.org>
2014-07-07 13:35:53 +0300

    clean the routes and move the functionality to the controllers

--- routes/university.js
+++ routes/university.js
@@ -4,25 +4,7 @@ var router = express.Router();
 var universityDb = require('../lib/db/university.js');
 
 router.get('/create', function(req, res) {
-    var universityName = req.query.name;
-
-    var j = {
-        ok: true,
-        name: ""
-    };
-
-    if (!universityName) {
-        j.ok = false;
-        res.json(j);
-    }
-
-    universityDb.create(function(success) {
-        if (success) {
-            j.name = universityName;
-            res.json(j);
-        }
-
-    }, universityName);
+    universityDb.create(req, res);
 });
 
 module.exports = router;
