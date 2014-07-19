71f358e5541b6cf45da752d3fcbf1fcce3fb8c75
Giorgos Tsiapaliokas <giorgos.tsiapaliokas@kde.org>
2014-07-19 18:35:17 +0300

    handle POST requests

--- test/testutils.js
+++ test/testutils.js
@@ -8,18 +8,21 @@ var config = ini.parse(fs.readFileSync(process.cwd() + '/diogenis.conf', 'utf-8'
 module.exports.getUrl = function(opts, cb) {
     var options = {
         url: 'http://' + config.server.server_host + ':' + config.server.server_port,
-        method: 'GET'
+        method: opts.method ? opts.method : 'GET'
     };
 
     options.url += '/' + opts.path;
 
     if (opts.qs) {
         options.qs = opts.qs;
+    } else if (opts.form) {
+        options.form = opts.form;
     }
 
     request(options, function (error, response, body) {
         if (!error) {
             response.statusCode.should.equal(200);
+            response.body = JSON.parse(response.body)
             cb(response, body);
         } else {
             console.log('Error in request!!!');
