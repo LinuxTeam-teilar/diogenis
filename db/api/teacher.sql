059f901e55ad484efe03d216ed58d184cd2c9a23
Giorgos Tsiapaliokas <giorgos.tsiapaliokas@kde.org>
2014-07-22 18:34:05 +0300

    replace exceptions with booleans
    Instead of using exceptions for handling errors we will use booleans
    or records. This change in necessary because node-postgres doesn't
    provide us access to those exceptions.

--- db/api/teacher.sql
+++ db/api/teacher.sql
@@ -1,25 +1,32 @@
-CREATE OR REPLACE FUNCTION teacher_create(teacherName text, teacherEmail text, teacherPassword text) RETURNS VOID AS $$
+CREATE OR REPLACE FUNCTION teacher_create(teacherName text, teacherEmail text, teacherPassword text) RETURNS BOOLEAN AS $$
 DECLARE
 BEGIN
     PERFORM email FROM teacher WHERE email = teacherEmail;
     IF FOUND THEN
-        RAISE EXCEPTION 'Teacher already exists!';
+        RETURN FALSE;
     END IF;
 
-    INSERT INTO teacher (name, email, password) VALUES (teacherName, teacherEmail, crypt(teacherPassword, gen_salt('md5')));
+    INSERT INTO teacher (name, email, password) VALUES
+                        (teacherName, teacherEmail, crypt(teacherPassword, gen_salt('md5')));
+    RETURN TRUE;
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE OR REPLACE FUNCTION teacher_auth(teacherEmail text, passwordCandidate text) RETURNS VOID AS $$
+CREATE OR REPLACE FUNCTION teacher_auth(teacherEmail text, passwordCandidate text) RETURNS BOOLEAN AS $$
 DECLARE
     teacherPassword text;
 BEGIN
     SELECT INTO teacherPassword password FROM teacher WHERE teacherEmail = email;
 
-    IF teacherPassword != crypt(passwordCandidate, teacherPassword) THEN
-        RAISE EXCEPTION 'Teacher Authorization has failed!';
+    IF NOT FOUND THEN
+        RETURN FALSE;
     END IF;
 
+    IF teacherPassword != crypt(passwordCandidate, teacherPassword) THEN
+        RETURN FALSE;
+    END IF;
+
+    RETURN TRUE;
 END;
 $$ LANGUAGE plpgsql;
 
