49c1f6c437fd268ad1817039d125fa47166e9c79
Giorgos Tsiapaliokas <giorgos.tsiapaliokas@kde.org>
2014-07-28 12:24:25 +0300

    add a new middleware for the routes
    the new middleware is called isSecretaryOrTeacher and it allows
    requests made only by the secretary or the teacher

--- routes/middleware.js
+++ routes/middleware.js
@@ -10,3 +10,13 @@ module.exports.isSecretary = function(req, res, next) {
     }
 };
 
+module.exports.isSecretaryOrTeacher = function(req, res, next) {
+    if (req.session && (req.session.isSecretary || req.session.isTeacher)) {
+        next();
+    } else {
+        var j = utils.errorJson(req, res, 'UnAuthorized');
+        res.statusCode = 401;
+        res.json(j);
+    }
+};
+
