9d9cc9ad453be937bd20506b235cc74f2bf37818
Giorgos Tsiapaliokas <giorgos.tsiapaliokas@kde.org>
2014-07-28 12:33:10 +0300

    add the business logic for lesson/create. Also this commit is an example of how to handle PL/pgSQL functions which returns json

---
+++ test/lesson.js
@@ -0,0 +1,138 @@
+var testUtils = require('./testutils.js');
+var should = require('should');
+
+describe('Lesson', function() {
+
+    describe('Create Lesson', function() {
+
+        it('Should succeed', function(done) {
+            testUtils.authSecretary(function(secretaryRes) {
+                var expected = {
+                    auth: {
+                        success: true
+                    },
+                    error: {
+                        id: -1,
+                        name: ''
+                    },
+                    lesson: {
+                        name: 'Programming 1',
+                        teacher: 1,
+                        department: 1
+                    }
+                };
+
+                var opts = {
+                    path: 'lesson/create',
+                    method: 'POST',
+                    auth: true
+                };
+
+                opts.form = {
+                    name: 'Programming 1',
+                    teacher: 1,
+                    department: 1
+                };
+
+                testUtils.getUrl(opts, function(res, body) {
+                    res.body.should.eql(expected)
+                    done();
+                });
+            });
+        });
+
+       it('Should Fail', function(done) {
+            testUtils.authSecretary(function(secretaryRes) {
+                var expected = {
+                    auth: {
+                        success: true
+                    },
+                    error: {
+                        id: 4,
+                        name: 'CreationFailed'
+                    }
+                };
+
+                var opts = {
+                    path: 'lesson/create',
+                    method: 'POST',
+                    auth: true
+                };
+
+                opts.form = {
+                    name: 'Programming 1',
+                    teacher: 1,
+                    department: 1
+                };
+
+                testUtils.getUrl(opts, function(res, body) {
+                    res.body.should.eql(expected)
+                    done();
+                });
+            });
+        });
+
+
+        it('Invalid Parameters', function(done) {
+            testUtils.authSecretary(function(secretaryRes) {
+                var expected = {
+                    auth: {
+                        success: true
+                    },
+                    error: {
+                        id: 3,
+                        name: 'InvalidParameters'
+                    }
+                };
+
+                var opts = {
+                    path: 'lesson/create',
+                    method: 'POST',
+                    auth: true
+                };
+
+                opts.form = {
+                    name: 'Programming 1',
+                    teacher: 1
+                    //department: 1
+                };
+
+                testUtils.getUrl(opts, function(res, body) {
+                    res.body.should.eql(expected)
+                    done();
+                });
+            });
+        });
+
+        it('UnAuthorized Request', function(done) {
+            var expected = {
+                auth: {
+                    success: false
+                },
+                error: {
+                    id: 2,
+                    name: 'UnAuthorized'
+                }
+            };
+
+            var opts = {
+                path: 'lesson/create',
+                method: 'POST',
+                statusCode: 401
+            };
+
+            opts.form = {
+                name: 'Programming 1',
+                teacher: 1,
+                department: 1
+            };
+
+            testUtils.getUrl(opts, function(res, body) {
+                res.body.should.eql(expected)
+                done();
+            });
+        });
+
+    });
+});
+
