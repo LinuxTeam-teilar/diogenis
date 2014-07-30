516bbd39c9fd64a7617bb3c5a11fbf3a8d34cac4
Giorgos Tsiapaliokas <giorgos.tsiapaliokas@kde.org>
2014-07-30 13:40:55 +0300

    remove student_update_password and port the functions to returning json

--- db/api/student.sql
+++ db/api/student.sql
@@ -1,50 +1,60 @@
-CREATE OR REPLACE FUNCTION student_create(studentName text, studentPassword text,
-                                          departmentId int, studentUsername text, studentIdentity text) RETURNS VOID AS $$
+CREATE OR REPLACE FUNCTION student_create_or_update(studentName text, studentPassword text,
+                                                    departmentId int, studentUsername text,
+                                                    studentIdentity text) RETURNS JSON AS $$
 DECLARE
+    studentRecord record;
 BEGIN
-    PERFORM username, identity FROM student WHERE username = studentUsername AND identity = studentIdentity;
+    PERFORM * FROM student WHERE username = studentUsername AND identity = studentIdentity
+                           AND departmentId = department AND studentName = name;
     IF FOUND THEN
-        RAISE EXCEPTION 'student already exists!';
+        --just update the password
+        UPDATE student SET password = crypt(studentPassword, gen_salt('md5')) WHERE username = studentUsername
+        RETURNING INTO studentRecord id, name, username, identity, department, email;
+    ELSE
+        -- the student doesn't exist, we must create him
+        INSERT INTO student (name, password, username, identity, department) VALUES
+            (studentName, crypt(studentPassword, gen_salt('md5')), studentUsername, studentIdentity, departmentId)
+            RETURNING INTO studentRecord id, name, username, identity, department, email;
     END IF;
 
-    INSERT INTO student (name, password, username, identity, department) VALUES
-           (studentName, crypt(studentPassword, gen_salt('md5')), studentUsername, studentIdentity, departmentId);
+    RETURN row_to_json(studentRecord);
+
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE OR REPLACE FUNCTION student_auth(studentUsername text, passwordCandidate text) RETURNS VOID AS $$
+CREATE OR REPLACE FUNCTION student_auth(studentUsername text, passwordCandidate text) RETURNS JSON AS $$
 DECLARE
     studentPassword text;
+    studentRecord record;
 BEGIN
     SELECT INTO studentPassword password FROM student WHERE username = studentUsername;
 
     IF studentPassword != crypt(passwordCandidate, studentPassword) THEN
-        RAISE EXCEPTION 'student Authorization has failed!';
+        SELECT INTO studentRecord ROW();
+    ELSE
+        SELECT INTO studentRecord id, name, username, department, identity, email
+        FROM student WHERE username = studentUsername;
     END IF;
+
+    RETURN row_to_json(studentRecord);
 
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE OR REPLACE FUNCTION student_update_email(studentUsername text, studentEmail text) RETURNS VOID AS $$
+CREATE OR REPLACE FUNCTION student_update_email(studentUsername text, studentEmail text) RETURNS JSON AS $$
 DECLARE
+    studentRecord record;
 BEGIN
-    UPDATE student SET email = studentEmail WHERE username = studentUsername;
+    PERFORM * FROM student WHERE username = studentUsername AND email = studentEmail;
 
     IF NOT FOUND THEN
-        RAISE EXCEPTION 'student does not exist, cannot update email!';
+        SELECT INTO studentRecord ROW();
+    ELSE
+        UPDATE student SET email = studentEmail WHERE username = studentUsername
+        RETURNING INTO studentRecord id, name, username, identity, department, email;
     END IF;
 
-END;
-$$ LANGUAGE plpgsql;
-
-CREATE OR REPLACE FUNCTION student_update_password(studentUsername text, studentPassword text) RETURNS VOID AS $$
-DECLARE
-BEGIN
-    UPDATE student SET password = crypt(studentPassword, gen_salt('md5')) WHERE username = studentUsername;
-
-    IF NOT FOUND THEN
-        RAISE EXCEPTION 'student does not exist, cannot update password!';
-    END IF;
+    RETURN row_to_json(studentRecord);
 
 END;
 $$ LANGUAGE plpgsql;
