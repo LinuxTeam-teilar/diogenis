858d6ef294fa11222d5723562007a1cfc95cf334
Giorgos Tsiapaliokas <giorgos.tsiapaliokas@kde.org>
2014-08-10 20:23:57 +0300

    use more sane names for classroom

--- db/schema/classroom.sql
+++ db/schema/classroom.sql
@@ -3,15 +3,15 @@ CREATE SEQUENCE seq_classroomIds;
 CREATE TABLE classroom
 (
     id                 int         primary key default nextval('seq_classroomIds'),
-    name               text        not null
+    name               text        unique not null
 );
 
 CREATE TABLE classroomSchedule
 (
     classroom    int    not null references classroom(id) on delete cascade,
     lesson       int    not null references lesson(id) on delete cascade,
-    start        int    check( > 0 ),
-    end          int    check( > 0 ),
-    day          int    check( > 0 AND <6 )
+    timeStart    int    check( timeStart > 0 ),
+    timeEnd      int    check( timeEnd > 0 ),
+    day          int    check( day > 0 AND day < 6 )
 );
 
