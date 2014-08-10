f493d45f5f07c1986854f573e1bd4a3ed99edccd
Giorgos Tsiapaliokas <giorgos.tsiapaliokas@kde.org>
2014-08-10 19:46:11 +0300

    finish with the coding of the student test. The test was missing some checks thats why it was disabled.

--- test/student.js
+++ test/student.js
@@ -2,27 +2,51 @@ var testUtils = require('./testutils.js');
 var should = require('should');
 var dionysosCredentials = testUtils.dionysosCredentials();
 
+function authStudent(done) {
+    var expected = {
+        "auth": {
+            "success": false
+        },
+        "error": {
+            "id": -1,
+            "name": ""
+        },
+        "student": {
+            "department": 1,
+            "email": null,
+            "id": 1,
+            "identity": dionysosCredentials.identity,
+            "name": dionysosCredentials.name,
+            "username": dionysosCredentials.username
+        }
+    };
+
+    var opts = {
+        path: 'student/auth',
+        method: 'POST'
+    };
+
+    opts.form = {
+        username: dionysosCredentials.username,
+        password: dionysosCredentials.password
+    };
+
+    testUtils.getUrl(opts, function(res, body) {
+        res.body.should.eql(expected)
+        done();
+    });
+}
+
 describe('Student', function() {
 
     describe('Auth Student', function() {
 
         it('Should succeed', function(done) {
-            var expected = {};
+           authStudent(done);
+        });
 
-            var opts = {
-                path: 'student/auth',
-                method: 'POST'
-            };
-
-            opts.form = {
-                username: dionysosCredentials.username,
-                password: dionysosCredentials.password
-            };
-
-            testUtils.getUrl(opts, function(res, body) {
-                res.body.should.eql(expected)
-                done();
-            });
+        it('Should succeed again', function(done) {
+           authStudent(done);
         });
 
         it('Student does not exist', function(done) {
