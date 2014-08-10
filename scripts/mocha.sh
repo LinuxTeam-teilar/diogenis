d3fb4ab97b401e1df761f8e49feacce2b4d48b0c
Giorgos Tsiapaliokas <giorgos.tsiapaliokas@kde.org>
2014-08-10 19:46:58 +0300

    add the student test and set the timeout of mocha to 0. Remote requests to dionysos need more than 200 ms which is the default

--- scripts/mocha.sh
+++ scripts/mocha.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-./node_modules/.bin/mocha --reporter $1 test/secretary.js \
-                                        test/teacher.js \
-                                        test/lesson.js \
-                                        test/semester.js \
-                                        test/student.js
+./node_modules/.bin/mocha -t 0 --reporter $1 test/secretary.js \
+                                             test/teacher.js \
+                                             test/lesson.js \
+                                             test/semester.js \
+                                             test/student.js
 
