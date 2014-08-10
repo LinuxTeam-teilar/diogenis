12fb9c86e44fb064b976a491be421fd1f8b8e65c
Giorgos Tsiapaliokas <giorgos.tsiapaliokas@kde.org>
2014-08-10 21:15:34 +0300

    fix brain dead way.
    Instead of returning a json with an integer just return an integer.
    0 is success and 1-6 is error.

--- db/api/classroom.sql
+++ db/api/classroom.sql
@@ -36,48 +36,30 @@ END;
 $$ LANGUAGE plpgsql;
 
 CREATE OR REPLACE FUNCTION classroom_add_lesson(classroomId int, lessondId int, startTime int,
-                                                endTime int, _day int) RETURNS JSON AS $$
+                                                endTime int, _day int) RETURNS INT AS $$
 DECLARE
    jsonResult json;
 BEGIN
-
-    CREATE TEMP TABLE result (
-        error int
-    ) ON COMMIT DROP;
-
-    INSERT INTO result (error) VALUES (0);
-
     PERFORM * FROM classroom WHERE id = classroomId;
     IF NOT FOUND THEN
-        INSERT INTO result (error) VALUES (1);
-        SELECT row_to_json(error.*) INTO jsonResult FROM error;
-        RETURN jsonResult;
+       RETURN 1;
     END IF;
 
     PERFORM * FROM lesson WHERE id = lessondId;
     IF NOT FOUND THEN
-        INSERT INTO result (error) VALUES (2);
-        SELECT row_to_json(error.*) INTO jsonResult FROM error;
-        RETURN jsonResult;
+        RETURN 2;
     END IF;
 
-
     IF startTime NOT BETWEEN 0 AND 23 THEN
-        INSERT INTO result (error) VALUES (3);
+        RETURN 3;
     END IF;
 
     IF endTime NOT BETWEEN 0 AND 23 THEN
-        INSERT INTO result (error) VALUES (34);
+        RETURN 4;
     END IF;
 
     IF _day NOT BETWEEN 0 AND 5 THEN
-        INSERT INTO result (error) VALUES (5);
-    END IF;
-
-    PERFORM * FROM result WHERE error > 0;
-    IF FOUND THEN
-        SELECT row_to_json(error.*) INTO jsonResult FROM error;
-        RETURN jsonResult;
+        RETURN 5;
     END IF;
 
     -- so our parameters are valid, lets continue by checking
@@ -90,17 +72,14 @@ BEGIN
           AND day = _day;
 
     IF FOUND THEN
-        INSERT INTO result (error) VALUES (6);
-        SELECT row_to_json(error.*) INTO jsonResult FROM error;
-        RETURN jsonResult;
+        RETURN 6;
     END IF;
 
     -- no errors so lets associate the classroom with the lesson
     INSERT INTO classroomSchedule (classroom, lesson, timeStart, timeEnd, day)
     VALUES (classroomId, lessondId, startTime, endTime, _day);
 
-    SELECT row_to_json(ROW()) INTO jsonResult;
-    RETURN jsonResult;
+    RETURN 0;
 
 END;
 $$ LANGUAGE plpgsql;
