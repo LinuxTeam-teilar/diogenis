7b30a62e77253c5d98ef0c6e1ebaffb7f6f373a4
Giorgos Tsiapaliokas <giorgos.tsiapaliokas@kde.org>
2014-08-11 14:58:32 +0300

    the student test must go first we need it for lesson

--- scripts/mocha.sh
+++ scripts/mocha.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-./node_modules/.bin/mocha -t 0 --reporter $1 test/secretary.js \
+./node_modules/.bin/mocha -t 0 --reporter $1 test/student.js \
+                                             test/secretary.js \
                                              test/teacher.js \
                                              test/lesson.js \
                                              test/semester.js \
-                                             test/classroom.js \
-                                             test/student.js
+                                             test/classroom.js
 
