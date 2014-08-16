c00391312fc625059574290d7423a37985ddf800
Giorgos Tsiapaliokas <giorgos.tsiapaliokas@kde.org>
2014-08-16 19:51:04 +0300

    reorder the tests

--- scripts/mocha.sh
+++ scripts/mocha.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-./node_modules/.bin/mocha -t 0 --reporter $1 test/student.js \
+./node_modules/.bin/mocha -t 0 --reporter $1 test/teacher.js \
+                                             test/student.js \
                                              test/secretary.js \
-                                             test/teacher.js \
                                              test/lesson.js \
                                              test/semester.js \
                                              test/classroom.js
