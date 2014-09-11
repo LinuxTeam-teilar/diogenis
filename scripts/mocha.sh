08bea957e52201ed95c4faa4143798565ac680a5
Giorgos Tsiapaliokas <giorgos.tsiapaliokas@kde.org>
2014-09-11 12:41:17 +0300

    change the timeout

--- scripts/mocha.sh
+++ scripts/mocha.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-./node_modules/.bin/mocha -t 0 --reporter $1 test/teacher.js \
-                                             test/student.js \
-                                             test/secretary.js \
-                                             test/lesson.js \
-                                             test/classroom.js \
-                                             test/lab.js \
-                                             test/teacher_list.js \
-                                             test/student_list.js
+./node_modules/.bin/mocha -t 100000 --reporter $1 test/teacher.js \
+                                                  test/student.js \
+                                                  test/secretary.js \
+                                                  test/lesson.js \
+                                                  test/classroom.js \
+                                                  test/lab.js \
+                                                  test/teacher_list.js \
+                                                  test/student_list.js
 
