d3b33a6c21111293705dd4e25918b65a93d993fc
Antonis Tsiapaliokas <antonis.tsiapaliokas@kde.org>
2014-09-23 18:01:08 +0300

    Add a new controller for the help support page

--- client/js/help.js
+++ client/js/help.js
@@ -6,4 +6,26 @@ diogenisControllers.controller('DiogenisHelpCtrl', ['$scope', '$routeParams', '$
   function($scope, $routeParams, $http, $route, $location, $filter, $cookieStore, GenerateFullName) {
   $scope.fullName = GenerateFullName;
 
+  $scope.alerts = []
+  $scope.closeAlert = function(index) {
+    $scope.alerts.splice(index, 1);
+  };
+
+  $scope.isUserLoggedIn = $cookieStore.get('fullName') !== undefined;
+  $scope.sendMail = function() {
+    //do something with the data.
+    console.log($scope.fullName)
+    console.log($scope.email)
+    console.log($scope.message)
+    console.log($scope.title)
+    //clear the previous alerts
+    $scope.alerts = [];
+    $scope.alerts.push({msg: 'Το email στάλθηκε επιτυχώς, θα επικοινωνήσουμε σύστομα μαζί σας.', type: 'success'});
+  }
+
+  $scope.resetForm = function() {
+    $scope.email = "";
+    $scope.message = "";
+    $scope.title = "";
+  }
   }]);
