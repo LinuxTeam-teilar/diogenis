5f269293a17848fb1d4315f4e2e45fef3ebb1ba7
Giorgos Tsiapaliokas <giorgos.tsiapaliokas@kde.org>
2014-09-25 13:50:51 +0300

    more test data

--- db/sample.sql
+++ db/sample.sql
@@ -1,3 +1,6 @@
 SELECT student_create_or_update('Giorgos Tsiapaliokas', '123', 1, 'giorgosTsiapaliokas', 'T0666');
 SELECT student_create_or_update('Antonis Tsiapaliokas', '321', 1, 'AntonisTsiapaliokas', 'T0777');
 
+SELECT student_create_or_update('Nurse Student', '123', 2, 'nursestudent', 'T0888');
+SELECT student_create_or_update('Nurse Student 2', '123', 2, 'nursestudent2', 'T0999');
+SELECT teacher_create('Nurse Teacher', 'nurseteacher@teilar.gr', '123', 2);
