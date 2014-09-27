421e21f56877fe5b01a3f03ee0738a46a9df6f36
Giorgos Tsiapaliokas <giorgos.tsiapaliokas@kde.org>
2014-09-27 19:36:22 +0300

    Don't use the hardcoded departmentId, instead take it from dionysos

--- db/defaultdata.sql
+++ db/defaultdata.sql
@@ -1,6 +1,6 @@
 SELECT university_create('T.E.I. Thessalias');
 
-SELECT department_create('T.P.T', 1);
+SELECT department_create('ΤΜΗΜΑ ΜΗΧΑΝΙΚΩΝ ΠΛΗΡΟΦΟΡΙΚΗΣ ΤΕ', 1);
 SELECT secretary_create('tpt-secretary', '1234567890', 1);
 
 SELECT department_create('Nurse', 1);
