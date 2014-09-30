85c18f83ad9f1b0b6cc868547952859deb955712
Giorgos Tsiapaliokas <giorgos.tsiapaliokas@kde.org>
2014-09-30 14:29:37 +0300

    Revert "Add real default data"
    This reverts commit 431cb7519a1efca365f266e962e45a16961641da.
    
    WE DON'T BREAK THE TESTS!!!

--- db/defaultdata.sql
+++ db/defaultdata.sql
@@ -1,46 +1,8 @@
 SELECT university_create('T.E.I. Thessalias');
 
 SELECT department_create('ΤΜΗΜΑ ΜΗΧΑΝΙΚΩΝ ΠΛΗΡΟΦΟΡΙΚΗΣ ΤΕ', 1);
-SELECT secretary_create('secry-cs', 'superpassword', 1);
+SELECT secretary_create('tpt-secretary', '1234567890', 1);
 
-SELECT department_create('ΤΜΗΜΑ ΛΟΓΙΣΤΙΚΗΣ ΚΑΙ ΧΡΗΜΑΤΟΟΙΚΟΝΟΜΙΚΗΣ', 1);
-SELECT secretary_create('secry-account', 'superpassword', 2);
+SELECT department_create('Nurse', 1);
+SELECT secretary_create('nurse-secretary', '1234567890', 2);
 
-SELECT department_create('ΤΜΗΜΑ ΤΕΧΝΟΛΟΓΩΝ ΓΕΩΠΟΝΩΝ', 1);
-SELECT secretary_create('secry-agric', 'superpassword', 3);
-
-SELECT department_create('ΤΜΗΜΑ ΔΙΟΙΚΗΣΗΣ ΕΠΙΧΕΙΡΗΣΕΩΝ - ΔΙΟΙΚΗΣΗ ΕΠΙΧΕΙΡΗΣΕΩΝ', 1);
-SELECT secretary_create('secry-business ', 'superpassword', 4);
-
-SELECT department_create('ΤΜΗΜΑ ΠΟΛΙΤΙΚΩΝ ΜΗΧΑΝΙΚΩΝ Τ.Ε. (ΤΡΙΚΑΛΑ)', 1);
-SELECT secretary_create('secry-civil-trikala', 'superpassword', 5);
-
-SELECT department_create('ΤΜΗΜΑ ΠΟΛΙΤΙΚΩΝ ΜΗΧΑΝΙΚΩΝ ΤΕ', 1);
-SELECT secretary_create('secry-civil', 'superpassword', 6);
-
-SELECT department_create('ΤΜΗΜΑ ΔΙΑΤΡΟΦΗΣ ΚΑΙ ΔΙΑΙΤΟΛΟΓΙΑΣ', 1);
-SELECT secretary_create('secry-diet', 'superpassword', 7);
-
-SELECT department_create('ΤΜΗΜΑ ΗΛΕΚΤΡΟΛΟΓΩΝ ΜΗΧΑΝΙΚΩΝ ΤΕ', 1);
-SELECT secretary_create('secry-electr', 'superpassword', 8);
-
-SELECT department_create('ΤΜΗΜΑ ΜΗΧΑΝΟΛΟΓΩΝ ΜΗΧΑΝΙΚΩΝ ΤΕ', 1);
-SELECT secretary_create('secry-engin', 'superpassword', 9);
-
-SELECT department_create('ΤΜΗΜΑ ΤΕΧΝΟΛΟΓΙΑΣ ΤΡΟΦΙΜΩΝ', 1);
-SELECT secretary_create('secry-food', 'superpassword', 10);
-
-SELECT department_create('ΤΜΗΜΑ ΔΑΣΟΠΟΝΙΑΣ & ΔΙΑΧΕΙΡΙΣΗΣ ΦΥΣΙΚΟΥ ΠΕΡΙΒΑΛΛΟΝΤΟΣ', 1);
-SELECT secretary_create('secry-forest', 'superpassword', 11);
-
-SELECT department_create('ΤΜΗΜΑ ΜΗΧΑΝΙΚΗΣ ΒΙΟΣΥΣΤΗΜΑΤΩΝ', 1);
-SELECT secretary_create('secry-machine', 'superpassword', 12);
-
-SELECT department_create('ΤΜΗΜΑ ΙΑΤΡΙΚΩΝ ΕΡΓΑΣΤΗΡΙΩΝ', 1);
-SELECT secretary_create('secry-medlab', 'superpassword', 13);
-
-SELECT department_create('ΤΜΗΜΑ ΝΟΣΗΛΕΥΤΙΚΗΣ', 1);
-SELECT secretary_create('secry-nurse', 'superpassword', 14);
-
-SELECT department_create('ΤΜΗΜΑ ΣΧΕΔΙΑΣΜΟΥ ΤΕΧΝΟΛΟΓΙΑΣ ΞΥΛΟΥ ΚΑΙ ΕΠΙΠΛΟΥ TE', 1);
-SELECT secretary_create('secry-wood', 'superpassword', 15);
